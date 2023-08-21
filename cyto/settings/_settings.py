2602ad6c5aafaf142cee21cd555098012ca9bf75
Frederik Aalund <fpa@sbtinstruments.com>
2023-08-21 08:11:31 +0000

    feat(settings): add 'override' kwarg to register and cache global settings

--- cyto/settings/_settings.py
+++ cyto/settings/_settings.py
@@ -9,10 +9,36 @@ _SETTINGS: dict[str, type[BaseModel]] = {}
 T = TypeVar("T", bound=BaseModel)
 
 
-def register(name: str) -> Callable[[type[T]], type[T]]:
-    """Register application wide settings class."""
+def register(
+    name: str, *, override: bool | None = None
+) -> Callable[[type[T]], type[T]]:
+    """Register application-wide settings class.
+
+    Raises `RuntimeError` if there already is a settings class for the given name
+    *unless* `override=True`.
+    Analogously, raises `RuntimeError` if there is no existing settings class for
+    the given name *and* `override=True`.
+    In other words, we are very strict about the use of the `override` paramater.
+    This is an effort to avoid common pitfalls such as accidental overrides or
+    non-effectual overrides.
+    """
+
+    if override is None:
+        override = False
 
     def _register(settings: type[T]) -> type[T]:
+        if name in _SETTINGS:
+            if not override:
+                raise RuntimeError(
+                    "There is already a settings class registered under the "
+                    f"'{name}' name. Use 'override=True' to suppress this error."
+                )
+        elif override:
+            raise RuntimeError(
+                "There is no existing settings class registered under the "
+                f"'{name}' name. In this case, it's an error to use 'override=True' "
+                "since there is nothing to override."
+            )
         _SETTINGS[name] = settings
         return settings
 
--- cyto/settings/_settings_factory.py
+++ cyto/settings/_settings_factory.py
@@ -1,6 +1,8 @@
 from collections.abc import Callable, Iterable
+from functools import cache
 from typing import TypeVar
 
+from pydantic import BaseSettings
 from pydantic.env_settings import SettingsSourceCallable
 
 from ..factory import CanNotProduce, ProductSpec
@@ -13,17 +15,36 @@ T = TypeVar("T")
 def settings_factory(
     *, app_name: str, extra_sources: Iterable[SettingsSourceCallable] = ()
 ) -> Callable[[ProductSpec[T]], T]:
-    def _settings_factory(spec: ProductSpec[T]) -> T:
-        if spec.annotation not in _SETTINGS.values():
-            raise CanNotProduce
-        cls = get_base_settings_class()
-        # TODO: LRU cache for both `auto_cls` and `all_settings`
-        auto_cls = autofill(app_name, extra_sources=extra_sources)(cls)
-        all_settings = auto_cls()
+    frozen_extra_sources = tuple(extra_sources)
+    all_settings = _get_all_settings(
+        app_name=app_name, extra_sources=frozen_extra_sources
+    )
 
+    def _settings_factory(spec: ProductSpec[T]) -> T:
+        # if spec.annotation not in _SETTINGS.values():
+        #    raise CanNotProduce
+
+        # TODO: Improve on this worst-case runtime of `O(n)`. For now,
+        # there are very few settings (`n` is small), so it's okay.
         for _, setting in all_settings:
             if isinstance(setting, spec.annotation):
                 return setting  # type: ignore[no-any-return]
         raise CanNotProduce
 
     return _settings_factory
+
+
+@cache
+def _get_all_settings(
+    *, app_name: str, extra_sources: tuple[SettingsSourceCallable, ...] = ()
+) -> BaseSettings:
+    auto_cls = _get_auto_cls(app_name=app_name, extra_sources=extra_sources)
+    return auto_cls()
+
+
+@cache
+def _get_auto_cls(
+    *, app_name: str, extra_sources: tuple[SettingsSourceCallable, ...] = ()
+) -> type[BaseSettings]:
+    cls = get_base_settings_class()
+    return autofill(app_name, extra_sources=extra_sources)(cls)
